0589d797a1fcd7196441d14c6b713ed38edfe2bd
Ian Cho <ha0511kr.com@gmail.com>
2017-05-08 22:53:11 -0600

    Compatiblilty transition with all internet browser

--- Javascript.js
+++ Javascript.js
@@ -8,23 +8,35 @@ function changeTitle()
     {
     document.getElementById("Main_title").style = "opacity: 1;"; 
     document.getElementById("Main_title").style.transition = "all 1s"; 
+    document.getElementById("Main_title").style.WebkitTransition = "all 1s";
+    document.getElementById("Main_title").style.MozTransition = "all 1s"; 
+    document.getElementById("Main_title").style.OTransition = "all 1s"; 
     document.getElementById("Main_title").innerHTML = "Scratch Day Scoreboard 1.0";
     }
     if(time > 9)
     {
     document.getElementById("Main_title").style = "opacity: 0;"; 
     document.getElementById("Main_title").style.transition = "all 1s"; 
+    document.getElementById("Main_title").style.WebkitTransition = "all 1s";
+    document.getElementById("Main_title").style.MozTransition = "all 1s"; 
+    document.getElementById("Main_title").style.OTransition = "all 1s"; 
     }
     if(time > 10)
     {
     document.getElementById("Main_title").style = "opacity: 1;"; 
     document.getElementById("Main_title").style.transition = "all 1s"; 
+    document.getElementById("Main_title").style.WebkitTransition = "all 1s";
+    document.getElementById("Main_title").style.MozTransition = "all 1s"; 
+    document.getElementById("Main_title").style.OTransition = "all 1s"; 
     document.getElementById("Main_title").innerHTML = "Total Score: ";  
     }
     if(time > 19)
     {
     document.getElementById("Main_title").style = "opacity: 0;"; 
-    document.getElementById("Main_title").style.transition = "all 1s";  
+    document.getElementById("Main_title").style.transition = "all 1s"; 
+    document.getElementById("Main_title").style.WebkitTransition = "all 1s";
+    document.getElementById("Main_title").style.MozTransition = "all 1s"; 
+    document.getElementById("Main_title").style.OTransition = "all 1s";  
     }
     if(time >= 20)
     {
