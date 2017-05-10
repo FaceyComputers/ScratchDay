08c380dc528e1248dba7d73e4568c9987c969aff
Ian Cho <ha0511kr.com@gmail.com>
2017-05-10 13:32:12 -0600

    Added php

--- pages/index.php
+++ pages/index.php
@@ -51,6 +51,18 @@
         <h2>Nick and Ian.</h2>
     </div>
     
+    <?php
+    $str = file_get_contents("../data/schoolData.json");
+    $json = json_decode($str, true);
+
+    for($a = 0; $a <= count($json['schools']) - 1; $a++)
+    {
+	$stringvariable = (string)$a;
+	$variables = $json['schools'][$stringvariable]['fullName'];
+	echo $variables . PHP_EOL;
+	}
+    ?> 
+    
 </body>
     <script type = "text/javascript" src = "../js/Javascript.js"></script>
     <script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
--- testing/getSchool.php
+++ testing/getSchool.php
@@ -6,8 +6,12 @@
 $str = file_get_contents("../data/schoolData.json");
 $json = json_decode($str, true);
 
-$variables = $json['schools']['CLB']['Beginner'];
-echo $variables . PHP_EOL;
+    for($a = 0; $a <= count($json['schools']) - 1; $a++)
+    {
+	$stringvariable = (string)$a;
+	$variables = $json['schools'][$stringvariable]['fullName'];
+	echo $variables . PHP_EOL;
+	}
 ?>
 
 </body>
