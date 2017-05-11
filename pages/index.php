943d5f45150e3fc9ef1d4942b6aca6f86681d9fe
Ian Cho <ha0511kr.com@gmail.com>
2017-05-11 16:37:16 -0600

    Changes

--- pages/index.php
+++ pages/index.php
@@ -51,18 +51,6 @@
         <h2>Nick and Ian.</h2>
     </div>
     
-    <?php
-    $str = file_get_contents("../data/schoolData.json");
-    $json = json_decode($str, true);
-
-    for($a = 0; $a <= count($json['schools']) - 1; $a++)
-    {
-	$stringvariable = (string)$a;
-	$variables = $json['schools'][$stringvariable]['fullName'];
-	echo $variables . PHP_EOL;
-	}
-    ?> 
-    
 </body>
     <script type = "text/javascript" src = "../js/Javascript.js"></script>
     <script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
