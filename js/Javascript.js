fe1bfba569f69f5c986662fb2814fb77b9447d1b
Ian Cho <ha0511kr.com@gmail.com>
2017-05-12 14:17:34 -0600

    Update Javascript.js

--- js/Javascript.js
+++ js/Javascript.js
@@ -34,7 +34,7 @@ function changeTitle()
     document.getElementById("Main_title").style.WebkitTransition = "all 1s";
     document.getElementById("Main_title").style.MozTransition = "all 1s"; 
     document.getElementById("Main_title").style.OTransition = "all 1s"; 
-    document.getElementById("Main_title").innerHTML = "Scratch Day Scoreboard 1.0";
+    document.getElementById("Main_title").innerHTML "EIPS Scratch Day";
     }
     if(time > 9)
     {
